210c5b08072e4a877d8be6a3cac3b0f981d6451d
Ratan Hussain <mdratanmia7395@gmail.com>
2023-11-21 15:42:44 +0300

    update

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ let fontSize = document.querySelector('#fontSize');
 let bgColor = document.querySelector('#bgColor');
 let textArea = document.querySelector('#textArea');
 
-function get(){
-    let getted = localStorage.getItem('fontSize')
-    textArea.style.fontSize = getted;
-
-}
 
 
 // fontSize listener here
 fontSize.addEventListener('change',(event)=>{
     let value = event.target.value
     localStorage.setItem('fontSize',value)
-    get()
+
 })
 
+function get(){
+    let getted = localStorage.getItem('fontSize');
+    textArea.style.fontSize = getted;
+    console.log(getted)
+
+}
 
 // Background color listener here
-bgColor.addEventListener('change',(event)=>{
-    // textArea.style.background = event.target.value;
-    localStorage.setItem('bgColor',event.target.value)
-    localStorage.getItem('bgColor')
-})
+// bgColor.addEventListener('change',(event)=>{
+//     // textArea.style.background = event.target.value;
+//     localStorage.setItem('bgColor',event.target.value)
+//     localStorage.getItem('bgColor')
+// })
+
+
+get();
